3778f23d8be0caf0a1400c99997b7df602253c75
= <=>
2022-10-31 22:52:40 +0700

    Add

--- src/ERC20.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Jucci1887\Ethereum;
-use Jucci1887\Ethereum\Foundation\StandardERC20Token;
-
-class ERC20 extends StandardERC20Token
-{
-    protected $contractAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7";
-}
